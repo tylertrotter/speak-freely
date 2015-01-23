5f2ecf50c71ebf37736e7475c10215af25bb1280
Tyler Trotter <tylertrotter@gmail.com>
2015-01-23 07:52:00 -0500

    This thing pretty much works now.

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
         <!-- <meta name="description" content=""> -->
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- <link rel="apple-touch-icon" href="apple-touch-icon.png"> -->
+        <link href='http://fonts.googleapis.com/css?family=Raleway:400,900,300' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="css/main.css">
         <!-- <script src="js/vendor/modernizr-2.8.3.min.js"></script> -->
     </head>
@@ -22,83 +23,92 @@
         <!--[if lt IE 8]>
             <p class="browserupgrade">You are using an outdated browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
         <![endif]-->
-    <section id="start">
-        <button class="start-round">Start Game</button>
-         <code>http://localhost/word-game?r=2&amp;s=10,20&amp;ct=0</code>
-    </section>
-    
-    <section id="confirm">
-        <div class="current-team">Team A</div>
-        <h1>Confirm Your Results</h1>
-        <ol></ol>
-        <div>
-            Total: <span id="round-points">0</span>
-        </div>
-        <button id="confirm-results">Looks Good</button>
-    </section>
-    
-    <section id="scoreboard">
-        <div class="score-container">
-            <div class="team-0">
-                <span class="team-0-name">Team A</span>
-                <span class="team-0-score">0</span>
-                <ol class="rounds">
-                    <li></li><li></li><li></li><li></li><li></li><li></li>
-                </ol>
-            </div>
-            <div class="team-1">
-                <span class="team-1-name">Team B</span>
-                <span class="team-1-score">0</span>
-                <ol class="rounds">
-                    <li></li><li></li><li></li><li></li><li></li><li></li>
-                </ol>
-            </div>
-        </div>
-        <button class="start-round">Start Next Round</button>
-        <div><span class="current-team">Team</span> are up next.</div>
-        
-        <hr>
-        
-        Save or share the current game.
-        <code>http://localhost/word-game?r=2&amp;s=10,20&amp;ct=0</code>
-        <input type="text" value="">
-    </section>
-    
-    <section id="gameover">
-        <h1>Game Over</h1>
-        <span id="winning-team">winning team</span> won!
-    </section>
-    
-    <section id="in-progress">
-        <div class="current-team">Team A</div>
-        <div id="timer">
-            <div></div>
-        </div>
-        
-        <section id="controls">
-            <div id="pass">
-                <button>X</button>
-                <div class="points-at-stake">-3</div>
-            </div>
+    <div class="wrapper">
+        <section id="start">
+            <h1>Speak Feely</h1>
+            <button class="start-round">Start Game</button>
 
-            <div id="correct">
-                <button>&gt;</button>
-                <div class="points-at-stake">6</div>
-            </div>
-        </section>
-        
-        <main id="word"></main>
-
-        <section id="restriction">
-            Forbidden: <span></span>
+            <section>
+                <h2>How to Play</h2>
+                <p>Get one or more teammates to guess as many words in 2 minutes as you can. You can do and say pretty much anything but say the word or words, spell it out ("It starts with a G") or say something like "It's Beorge Bashington!" That's not clever, that's cheating.</p>
+                <h3>Scoring</h3>
+                <p>If you're stumped you can pass, but there will be a penalty. The harder a word is, the more points you'll get for it and the less your penalty will be if you decide to pass.</p>
+                <h3>Rounds</h3>
+                <p>There are 6 rounds for each team, the team with the most points at the end of 6 rounds wins. <span class="team-0-name"></span> go first, then the <span class="team-1-name"></span>.</p>
+            </section>
         </section>
 
-        
-    </section>
-    
+        <section id="confirm">
+            <div class="current-team">Team A</div>
+            <h1>Confirm Your Results</h1>
+            <ol></ol>
+            <div>
+                Total: <span id="round-points">0</span>
+            </div>
+            <button id="confirm-results">Looks Good</button>
+        </section>
+
+        <section id="scoreboard">
+            <div class="score-container">
+                <div class="team-0">
+                    <span class="team-0-name">Team A</span>
+                    <span class="team-0-score">0</span>
+                    <ol class="rounds">
+                        <li></li><li></li><li></li><li></li><li></li><li></li>
+                    </ol>
+                </div>
+                <div class="team-1">
+                    <span class="team-1-name">Team B</span>
+                    <span class="team-1-score">0</span>
+                    <ol class="rounds">
+                        <li></li><li></li><li></li><li></li><li></li><li></li>
+                    </ol>
+                </div>
+            </div>
+            <button class="start-round">Start Next Round</button>
+            <div><span class="current-team">Team</span> are up next.</div>
+
+            <hr>
+
+            <button class="new-game">New Game</button>
+        </section>
+
+        <section id="gameover">
+            <h1>Game Over</h1>
+            <span id="winning-team">winning team</span> won!
+        </section>
+
+        <section id="in-progress">
+            <div class="current-team">Team A</div>
+            <div id="timer">
+                <div></div>
+            </div>
+
+            <section id="controls">
+                <div id="pass">
+                    <button>X</button>
+                    <div class="points-at-stake">-3</div>
+                </div>
+
+                <div id="correct">
+                    <button>&gt;</button>
+                    <div class="points-at-stake">6</div>
+                </div>
+            </section>
+
+            <main id="word"></main>
+
+            <section id="restriction">
+                Forbidden: <span></span>
+            </section>
+
+
+        </section>
+    </div>
     <div id="score">
         <button id="show-score">Score V</button>
         <div class="score-container">
+            <div class="pending">Points Pending</div>
             <div class="team-0">
                 <div class="team-0-name">Team Name</div>
                 <div class="score team-0-score">0</div>
@@ -115,17 +125,18 @@
             </div>
         </div>
     </div>
+</html>
    <!-- <script src="js/zepto.js"></script>-->
         <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <!--     <script>window.jQuery || document.write('<script src="js/vendor/jquery-2.1.3.min.js"><\/script>')</script>-->
+        <script>window.jQuery || document.write('<script src="js/jquery-2.1.3.min.js"><\/script>')</script>
     <script>
         function startRound(){
-            $('body').find('>section').hide();
+            $('.wrapper').find('>section').hide();
             getWord();
             $('#in-progress').show();
             $('#confirm').find('ol').html('');
             $('#timer').find('div').css('width', 0);
-            timer(.03 * 60 * 1000);
+            timer(.02 * 60 * 1000);
         }
         function switchCurrentTeam(){
             var teams = $('body').attr('data-teams').split(',');
@@ -173,16 +184,23 @@
             
             calcRoundTotal();
             
-            $('body').find('>section').hide();
+            var ding = document.createElement('audio');
+            ding.setAttribute('src', 'ding.mp3');
+            ding.setAttribute('autoplay', 'autoplay');
+            ding.load();
+            
+            $('.wrapper').find('>section').hide();
             $('#confirm').show();
+            window.location.hash = 'pending';
         }
         function goToScoreBoard(){
             var points = $('#round-points').text() * 1;
             var teamClassName = '.team-' + $('body').attr('data-current-team') + '-score';
             switchCurrentTeam();
             calcScore(teamClassName,points);
-            incrementRound();
-            $('body').find('>section').hide();
+            incrementRound(); 
+            $('.url').text();
+            $('.wrapper').find('>section').hide();
             if( $('body').attr('data-rounds') === '12' ){
                 if($('#score').find('.team-0-score').text()*1 > $('#score').find('.team-1-score').text()*1){
                     $('#winning-team').text($('#score').find('.team-0-name').text());        
@@ -191,7 +209,16 @@
                 }
                $('#gameover').show(); 
             }else{
-                $('#scoreboard').show();
+                var $body = $('body');
+                var rounds = $body.attr('data-rounds')*1 - 1;
+                var currentTeam = Math.abs($body.attr('data-current-team')*1-1);
+                var score = $('#scoreboard').find('.team-0-score').text() + ',' + $('#scoreboard').find('.team-1-score').text();
+                    var baseUrl = window.location.href.split('#')[0];    
+                    baseUrl = baseUrl.split('?')[0];
+                var url = baseUrl + '?r=' + rounds + '&s=' + score + '&ct=' + currentTeam;
+                window.location.replace(url);
+                //$('.url').text(url);
+                //$('#scoreboard').show();
             }
         }
         function calcRoundTotal(){
@@ -214,14 +241,19 @@
             var roundNum = $('body').attr('data-rounds')*1;   
             roundNum++;
             $('.score-container').each(function(){
-               $(this).find('.team-0').find('.rounds').find('li').eq( Math.ceil(roundNum/2)-1 ).addClass('completed');
-                if( Math.floor(roundNum/2)-1 >= 0){
-                    $(this).find('.team-1').find('.rounds').find('li').eq( Math.floor(roundNum/2)-1 ).addClass('completed');
-                } 
+                $(this).find('.team-0').find('.rounds').find('li').eq( Math.ceil(roundNum/2) ).prevAll().addClass('completed');
+                $(this).find('.team-1').find('.rounds').find('li').eq( Math.floor(roundNum/2) ).prevAll().addClass('completed');
             });
             
             $('body').attr('data-rounds', roundNum);
         }
+        $(document).on('click', '.new-game', function(){
+            var wantsNewGame = confirm('Do you want to abandon this game?');
+            if( wantsNewGame ){
+                window.location = window.location.href.split('?')[0];
+            }
+        });
+              
         $(document).on('click', '.start-round', function(){
              startRound();   
         });
@@ -233,6 +265,8 @@
             var passPoints = $('#pass').find('.points-at-stake').text();
             var correctPoints = $('#correct').find('.points-at-stake').text();
             var tallyHtml;
+            
+            $('#score').find('.pending').show();
  
            $('#confirm').find('ol').append('<li class="' + $(this).attr('id') + '">' + $('#word').text() + '<button data-confirm="pass" data-points="' + $('#pass').find('.points-at-stake').text() + '"></button><button data-confirm="correct" data-points="' + $('#correct').find('.points-at-stake').text() + '"></button></li>');
             
@@ -241,6 +275,7 @@
         });
   
         $('#confirm').on('click', '#confirm-results', function(){
+            $('#score').find('.pending').hide();
              goToScoreBoard();   
         });
         
@@ -279,12 +314,15 @@
             
             switchCurrentTeam();
             <?php
-            if (isset($_GET['r'])) {
-                echo "$('#scoreboard').show();"; 
-            }else{
-                echo "$('#start').show();";
-            }
-            ?>
+            if (isset($_GET['r'])) { ?>
+                incrementRound();
+                $('#scoreboard').show();
+                if(window.location.hash == '#pending') {
+                    alert('The last round was not confirmed. Confirm it before continuing.');
+                }
+            <?php }else{ ?>
+                $('#start').show();
+            <?php } ?>
         });
         
         /*
